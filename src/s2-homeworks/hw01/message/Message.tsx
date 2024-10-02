9dd57cff018ede349dc21801b6a66e07a8980433
Maksim Lebedev <monk527@yandex.ru>
2024-10-03 00:59:05 +0500

    add style for components Message

--- src/s2-homeworks/hw01/message/Message.tsx
+++ src/s2-homeworks/hw01/message/Message.tsx
@@ -1,45 +1,31 @@
-import React from 'react'
-import s from './Message.module.css'
-import avatar from '../avatar.png'
-import { MessageType } from '../HW1'
+import React from "react";
+import s from "./Message.module.css";
+import avatar from "../avatar.png";
+import { MessageType } from "../HW1";
 
-// нужно создать правильный тип вместо any
 export type MessagePropsType = {
-    message: MessageType
-}
+  message: MessageType;
+};
 
-// нужно отобразить приходящие данные
-const Message = ({message}: MessagePropsType) => {
-    return (
-        <div id={'hw1-message-' + message.id} className={s.message}>
-            <div className={s.imageAndText}>
-                <img
-                    id={'hw1-avatar-' + message.id}
-                    // создаёт студент
-                    // src={avatar}
-                    src={message.user.avatar}
-                    //
-                />
-                <div className={s.text}>
-                    <div id={'hw1-name-' + message.id} className={s.name}>
-                        {/*создаёт студент*/}
-                        {message.user.name}
-                        {/**/}
-                    </div>
-                    <pre id={'hw1-text-' + message.id} className={s.messageText}>
-                        {/*создаёт студент*/}
-                        {message.message.text}
-                        {/**/}
-                    </pre>
-                </div>
-            </div>
-            <div id={'hw1-time-' + message.id} className={s.time}>
-                {/*создаёт студент*/}
-                {message.message.time}
-                {/**/}
-            </div>
+const Message = ({ message }: MessagePropsType) => {
+  return (
+    <div id={"hw1-message-" + message.id} className={s.message}>
+      <div className={s.imageAndText}>
+        <img id={"hw1-avatar-" + message.id} src={message.user.avatar} />
+        <div className={s.text}>
+          <div id={"hw1-name-" + message.id} className={s.name}>
+            {message.user.name}
+          </div>
+          <pre id={"hw1-text-" + message.id} className={s.messageText}>
+            {message.message.text}
+          </pre>
         </div>
-    )
-}
+      </div>
+      <div id={"hw1-time-" + message.id} className={s.time}>
+        {message.message.time}
+      </div>
+    </div>
+  );
+};
 
-export default Message
+export default Message;
